22f2a05eea11db9c66efa1805c4f97cab8ac8da0
David Boudin <david.boudin@eleves.enpc.fr>
2019-03-18 13:44:12 +0100

    changements
    changements

--- Application/src/app/Conjugaison/Conjugaison.page.ts
+++ Application/src/app/Conjugaison/Conjugaison.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
-import { Platform } from '@ionic/angular';
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-Conjugaison',
@@ -16,7 +14,16 @@ export class ConjugaisonPage implements OnInit {
   public temps: string;
   public personne: string;
   public listeVerbe: Array<{ verbe: string}> = [];
+  public json = $.getJSON('./Verbes.json');
+  public data = eval(this.json.responseText);
+  public i = 0;
   constructor (private router: Router) {
+    for ( let key in this.data) {
+      if ( this.i = 1 ) {
+        console.log(this.data);
+      }
+      this.i = this.i+1;
+    }
   }
   ngOnInit(): void {
     this.listeVerbe = [];
